989111cec186b5e9c3c068a334e2053c71b4b271
BasiaB23 <barbara.bogut@gmail.com>
2017-08-30 22:42:46 +0200

    FirstCommit

---
+++ Strona salonu/assets/js/script.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+
+document.getElementById('toggle-menu').addEventListener('click', function(){
+  document.getElementById('menu').classList.toggle('open');
+});
+
+
+
+
+
+
+
+});
